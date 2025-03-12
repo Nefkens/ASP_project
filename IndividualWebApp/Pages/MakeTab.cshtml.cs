bccf072f55bc362cf3abcb242968b01a9251f226
Tigo <tigonefkens@gmail.com>
2025-03-12 11:42:10 +0100

    Added the razorpages application

---
+++ IndividualWebApp/Pages/MakeTab.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IndividualWebApp.Pages
+{
+    public class MakeTabModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ IndividualWebApp/wwwroot/js/scrollingTab.js
@@ -0,0 +1,12 @@
+﻿const scrollContainer = document.querySelector('.scroll-container');
+
+scrollContainer.innerHTML += scrollContainer.innerHTML;
+
+function autoScroll() {
+    scrollContainer.scrollLeft += 1;
+    if (scrollContainer.scrollLeft >= scrollContainer.scrollWidth / 2) {
+        scrollContainer.scrollLeft = 0;
+    }
+}
+
+setInterval(autoScroll, 20);
