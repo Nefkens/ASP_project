f6e6f15a1a2a9200f04b3f359ac80ba1d28bf4a0
Tigo <tigonefkens@gmail.com>
2025-04-17 15:29:56 +0200

    adding 3 layer architecture

---
+++ DAL/Data/DatabaseServices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Data
+{
+    internal class DatabaseServices
+    {
+        private string _connectionString;
+
+        public DatabaseServices(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void RegisterUser()
+        {
+            using var 
+        }
+    }
+}
